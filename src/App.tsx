9db656be6c441bb535129e3d8f1a22b463d4f747
drewberrysteph <drewberrysteph@gmail.com>
2025-08-03 20:02:21 +0800

    add completed and remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+type Todo = {
+  id: string;
+  title: string;
+  completed: boolean;
+};
+
+const todoFixtures = [
+  {
+    id: 1,
+    title: "Take out the trash",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Walk the dog",
+    completed: false,
+  },
+];
+
+const App = () => {
+  const [todos, setTodos] = useState(todoFixtures);
+
+  const handleCompleted = (id: number) => {
+    const newTodos = todos.map((todo) => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+
+    setTodos(newTodos);
+  };
+
+  const handleRemove = (id: number) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo</h1>
+      {todos.map((todo) => {
+        return (
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => handleCompleted(todo.id)}
+            />
+            <p style={{textDecoration: todo.completed ? "line-through" : ""}}>{todo.title}</p>
+            <button onClick={() => handleRemove(todo.id)}>
+              Remove
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
