9ca65365e16e2bc148d758726031ec6341276496
drewberrysteph <drewberrysteph@gmail.com>
2025-08-06 21:20:48 +0800

    add todo input

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 
 type Todo = {
   id: string;
@@ -21,38 +21,67 @@ const todoFixtures = [
 
 const App = () => {
   const [todos, setTodos] = useState(todoFixtures);
+  const [newTodo, setNewTodo] = useState({
+    id: 0,
+    title: "",
+    completed: false,
+  });
 
   const handleCompleted = (id: number) => {
     const newTodos = todos.map((todo) => {
-      if(todo.id === id){
-        todo.completed = !todo.completed
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
       }
-      return todo
-    })
+      return todo;
+    });
 
     setTodos(newTodos);
   };
 
   const handleRemove = (id: number) => {
     const newTodos = todos.filter((todo) => todo.id !== id);
-    setTodos(newTodos)
+    setTodos(newTodos);
+  };
+
+  const handleAddTodo = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setNewTodo({
+      id: todos.length + 1,
+      title: value,
+      completed: false,
+    });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setTodos((todos) => [...todos, newTodo]);
   };
 
   return (
     <div>
       <h1>Todo</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="add new todo"
+          value={newTodo.title}
+          onChange={handleAddTodo}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <hr />
       {todos.map((todo) => {
         return (
-          <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ display: "flex", alignItems: "center" }} key={todo.id}>
             <input
               type="checkbox"
               checked={todo.completed}
               onChange={() => handleCompleted(todo.id)}
             />
-            <p style={{textDecoration: todo.completed ? "line-through" : ""}}>{todo.title}</p>
-            <button onClick={() => handleRemove(todo.id)}>
-              Remove
-            </button>
+            <p style={{ textDecoration: todo.completed ? "line-through" : "" }}>
+              {todo.title}
+            </p>
+            <button onClick={() => handleRemove(todo.id)}>Remove</button>
           </div>
         );
       })}
